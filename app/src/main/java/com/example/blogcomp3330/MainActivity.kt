bc82b7fbcd3c27ef0a3c254f2d4d454cfe17cade
yawjalik <jalik2129@gmail.com>
2023-12-02 22:31:36 +0800

    Move PostsAdapter to separate file

--- app/src/main/java/com/example/blogcomp3330/MainActivity.kt
+++ app/src/main/java/com/example/blogcomp3330/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.blogcomp3330
 
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
@@ -179,50 +176,5 @@ class MainActivity : AppCompatActivity() {
                 // Update recycler view
                 recyclerView.adapter?.notifyDataSetChanged()
             }
-    }
-}
-
-class PostsAdapter(private val posts: List<Post>) :
-    RecyclerView.Adapter<PostsAdapter.ViewHolder>() {
-
-    private val storage = Firebase.storage
-
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val author: TextView = itemView.findViewById(R.id.postAuthor)
-        val title: TextView = itemView.findViewById(R.id.postTitle)
-        val content: TextView = itemView.findViewById(R.id.postContent)
-        val date: TextView = itemView.findViewById(R.id.postDate)
-        val image: ImageView = itemView.findViewById(R.id.postImage)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.post, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val post = posts[position]
-        holder.author.text = post.author
-        holder.title.text = post.title
-        holder.date.text = post.date
-        holder.content.text = post.content
-
-        // Download image
-        if (post.image != "") {
-            val imageRef = storage.reference.child(post.image)
-            val ONE_MEGABYTE: Long = 1024 * 1024
-            imageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener { bytes ->
-                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-                holder.image.setImageBitmap(bitmap)
-            }.addOnFailureListener {
-                Log.e("MainActivity", "Failed to get image")
-            }
-        } else {
-            holder.image.setImageResource(R.drawable.ic_launcher_foreground)
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return posts.size
     }
 }
